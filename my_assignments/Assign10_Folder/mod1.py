6c77382ccf1432b3707986a8494862a6234f1c67
Paritosh Nichat <paritoshnichat@gmail.com>
2021-08-01 10:34:31 +0530

    Assignment 10 - Submitting the Theory Assignment 10

---
+++ my_assignments/Assign10_Folder/mod1.py
@@ -0,0 +1,8 @@
+def fn1():
+    print("This is function 1")
+
+def fn2():
+    print("This is function 2")
+
+def fn3():
+    print("This is function 3")
---
+++ my_assignments/Assign10_Folder/mod2.py
@@ -0,0 +1,8 @@
+def fn21():
+    print("This is function 21")
+
+def fn22():
+    print("This is function 22")
+
+def fn23():
+    print("This is function 23")
